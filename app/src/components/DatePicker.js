1396f517f83793806636246bbb4382242d672855
Rodrigo Nehring <rodrigonehring@hotmail.com>
2020-05-06 18:21:07 -0300

    fix datepicker & upgrade

--- app/src/components/DatePicker.js
+++ app/src/components/DatePicker.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import enLocale from 'date-fns/locale/en-US'
 import DateFnsAdapter from '@material-ui/pickers/adapter/date-fns'
 import { DatePicker, LocalizationProvider } from '@material-ui/pickers'
-import { Typography } from '@material-ui/core'
+import { Typography, TextField } from '@material-ui/core'
 import { useField } from 'formik'
 
 function DatePickerFormik({ name, label }) {
@@ -10,9 +10,9 @@ function DatePickerFormik({ name, label }) {
   const [selectedDate, setValue] = useState(field.value ? new Date(field.value) : null)
 
   const handleDateChange = (date) => {
-    const isValid = !isNaN(date)
+    const isValid = date && !isNaN(date)
     setValue(date)
-    if (isValid) form.setValue(date.toISOString())
+    form.setValue(isValid ? date.toISOString() : null)
   }
 
   return (
@@ -21,10 +21,9 @@ function DatePickerFormik({ name, label }) {
         {...field}
         value={selectedDate}
         onChange={handleDateChange}
+        renderInput={(props) => <TextField {...props} variant="outlined" />}
+        onError={console.warn}
         label={label}
-        error={Boolean(meta.error)}
-        variant="outlined"
-        placeholder="01/01/2000"
         inputFormat="dd/MM/yyyy"
         mask="__/__/____"
       />
